f8853628e2dc3451072e53a9f085c63d5d35d74c
Carlos Guia <carlos.guia.a@gmail.com>
2024-06-17 18:46:40 -0300

    tercera entrega

---
+++ js/carrito.js
@@ -0,0 +1,106 @@
+let carrito = cargarCarrito();
+
+//Secciones del HTML
+let sectionPaquetes = document.getElementById("section-paquetes");
+let sectionCarrito = document.getElementById("section-carrito");
+
+//DOM
+let totalCompra = document.createElement("div");
+totalCompra.innerHTML = "<h2>Total: $</h2>";
+sectionCarrito.appendChild(totalCompra);
+
+let montoTotalCompra = document.createElement("h2");
+montoTotalCompra.innerText = "0";
+totalCompra.appendChild(montoTotalCompra);
+
+let cantidadPaquetes = document.createElement("div");
+cantidadPaquetes.innerHTML = "<h3>Paquetes: </h3>";
+sectionCarrito.appendChild(cantidadPaquetes);
+
+let cantPaquetes = document.createElement("h3");
+cantPaquetes.innerText = "0";
+cantidadPaquetes.appendChild(cantPaquetes);
+
+let botonFinalizar = document.createElement("button");
+botonFinalizar.innerText = "Finalizar compra";
+sectionCarrito.appendChild(botonFinalizar);
+botonFinalizar.setAttribute("class", "boton");
+
+
+//Boton de pago.
+botonFinalizar.onclick = () => {
+  const precioFinal = montoTotalCompra.innerText;
+  //Estetica 
+  Swal.fire({
+    title: '¿Deseas finalizar tu compra?',
+    text: `Total a pagar: $${precioFinal}`,
+    showCancelButton: true,
+    confirmButtonColor: '#008f39',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si',
+    cancelButtonText: 'No'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Compra confirmada',
+        '¡Feliz viaje!',
+        'success'
+      )
+      vaciarCarrito();
+    }
+  })
+}
+
+
+//Seccion de cartas
+for (const paquete of paquetes) {
+  let container = document.createElement("div");
+  container.setAttribute("class", "card-paquete");
+  container.innerHTML = ` <div class="img-container">
+                            <img src="${paquete.foto}" alt="${paquete.nombre}" class="img-paquete"/>
+                            </div>
+                            <div class="info-paquete">
+                            <p class="font">${paquete.nombre}</p>
+                            <strong class="font">$${paquete.precio}</strong>
+                            <button class="boton" id="btn${paquete.id}"> Agregar al carrito </button>
+                            </div>`;
+  sectionPaquetes.appendChild(container);
+  //Evento para que los paquetes se agreguen al carrito al hacer click en el boton
+  document.getElementById(`btn${paquete.id}`).onclick = () => agregarAlCarrito(`${paquete.id}`);
+}
+
+
+
+//Funciones
+function agregarAlCarrito(id) {
+  carrito.push(paquetes.find(p => p.id == id));
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  calcularTotalCarrito();
+}
+
+function calcularTotalCarrito() {
+  let total = 0;
+  for (const paquete of carrito) {
+    total += paquete.precio;
+  }
+  montoTotalCompra.innerText = total;
+  cantPaquetes.innerText = carrito.length;
+}
+
+function vaciarCarrito() {
+  montoTotalCompra.innerText = "0";
+  cantPaquetes.innerText = "0";
+  localStorage.clear();
+  carrito = [];
+}
+
+
+function cargarCarrito() {
+  let carrito = JSON.parse(localStorage.getItem("carrito"));
+  if (carrito == null) {
+    return [];
+  } else {
+    return carrito;
+  }
+}
+
---
+++ js/paquetes.js
@@ -0,0 +1,8 @@
+const paquetes = [
+    {id:01, tipo:"Paquete", nombre:"Transporte Aereo Turista", precio: 4500, foto: "./media/1.jpg" },
+    {id:02, tipo:"Paquete", nombre:"Trnasporte Aereo Privado", precio: 20000, foto: "./media/2.jpg" },
+    {id:03, tipo:"Paquete", nombre:"Transporte Terrestre", precio: 1500, foto: "./media/3.jpg" },
+    {id:04, tipo:"Paquete", nombre:"Transporte Maritimo Privado", precio: 10000, foto: "./media/4.jpg" },
+    {id:05, tipo:"Dulce", nombre:"Snacks", precio: 25, foto: "./media/5.jpg" },
+    {id:06, tipo:"Dulce", nombre:"Dulces", precio: 25, foto: "./media/6.jpg" },
+];
